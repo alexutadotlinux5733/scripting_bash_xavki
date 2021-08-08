de9968bc5eca9f64af9201b15768432bb7287f9b
alexutadotlinux5733 <boudreaultarthur@gmail.com>
2021-08-08 13:35:24 +0200

    [DEV] ajout commande pour option --create et --drop

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-# Si option --create alors on fait quelque chose sinon affichage aide
-clear
+# Si option --create alors on lance crée un conteneur alpine
+# Si on ajoute un argument
+
 if [[ $1 == "--create"  ]]; then
-  echo "do $1"
-# Si option --drop alors faire quelque chose
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
+  read -p " Combien de conteneurs voulez-vous déployer ? : " nb_container
+#  docker run -tid --name $USER-alpine alpine:latest
+  echo "J'ai crée ${nb_container} conteneurs"
+
 elif [[ $1 == "--drop" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
+
+  docker rm -f $USER-alpine
+
 elif [[ $1 == "--infos" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
 elif [[ $1 == "--start" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
 elif [[ $1 == "--ansible" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
 else
-  echo "
+  echo -e "
+
+    Syntaxe : ./deploy.sh --\e[3moption\e[0m
 
     Options :
       - --create: lancer les conteneurs
