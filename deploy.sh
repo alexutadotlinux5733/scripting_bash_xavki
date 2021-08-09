0bbbe06b717414ea188ae716bd2945335dd280b0
alexutadotlinux5733 <57273277+alexutadotlinux5733@users.noreply.github.com>
2021-08-09 19:14:06 +0200

    [DEV] ajout boucle for à --create et modif --drop

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,43 @@
 #!/bin/bash
 
-# Si option --create alors on fait quelque chose sinon affichage aide
-clear
+# Si option --create alors on lance crée un conteneur alpine
+# Ensuite on demande à l'utilisateur le nombre de conteneurs qu'il souhaite créer
+# Utilisation d'une boucle FOR pour créer plusieurs conteneurs et leur attribuer des noms uniques
+
 if [[ $1 == "--create"  ]]; then
-  echo "do $1"
-# Si option --drop alors faire quelque chose
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
+  read -p " Combien de conteneurs voulez-vous déployer ? : " nb_container
+  for i in $(seq 1 $nb_container); do
+    docker run -tid --name $USER-alpine-$i alpine:latest
+    echo "Conteneur $USER-alpine-$i crée"
+  done
+
+  echo "J'ai crée ${nb_container} conteneurs"
+
 elif [[ $1 == "--drop" ]]; then
-  echo "do $1"
+  echo && echo "Vous avez choisi l'option $1"
+  echo && docker ps
+  echo && read -p "Quel conteneur souhaitez-vous supprimer ? " name_container
+  docker rm -f $name_container
+
 elif [[ $1 == "--infos" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
 elif [[ $1 == "--start" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
 elif [[ $1 == "--ansible" ]]; then
-  echo "do $1"
+  echo
+  echo "Vous avez choisi l'option $1"
+  echo
 else
-  echo "
+  echo -e "
+
+    Syntaxe : ./deploy.sh --\e[3moption\e[0m
 
     Options :
       - --create: lancer les conteneurs
